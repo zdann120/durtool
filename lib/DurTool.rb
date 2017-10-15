e4a11e6d0e5e5d37da8b223cbfa1e7f59e6594a9
Zach Nusbaum <zachdnusbaum@gmail.com>
2017-10-14 23:44:14 -0700

    Add advanced mode

--- lib/DurTool.rb
+++ lib/DurTool.rb
@@ -1,7 +1,29 @@
 require "DurTool/version"
 
 module DurTool
-  # Your code goes here...
+  class Duration
+    attr_reader :total_seconds, :days, :hours, :minutes, :seconds
+    def initialize(total_seconds)
+      @total_seconds = total_seconds
+      @days = @total_seconds / (24 * 60 * 60)
+      @hours = (@total_seconds / (60 * 60)) % 60
+      @minutes = (@total_seconds / 60) % 60
+      @seconds = @total_seconds % 60
+    end
+
+    def to_h
+      {
+        days: @days,
+        hours: @hours,
+        minutes: @minutes,
+        seconds: @seconds
+      }
+    end
+
+    def to_json
+      to_h.to_json
+    end
+  end
   def self.format(total_seconds)
     total_seconds = total_seconds.to_i
     days = total_seconds / (24 * 60 * 60)
